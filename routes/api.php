7c1052673eb8f856ad587249fb0fe3b5c92a5978
roneydev01 <roneyfreitas.rf@gmail.com>
2021-09-05 00:17:38 -0300

    feat: Installed and configured JWT-Auth

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,8 @@ Route::apiResource('carro', CarroController::class);
 Route::apiResource('locacao', LocacaoController::class);
 Route::apiResource('marca', MarcaController::class);
 Route::apiResource('modelo', ModeloController::class);
+
+Route::post('login', 'AuthController@login');
+Route::post('logout', 'AuthController@logout');
+Route::post('refresh', 'AuthController@refresh');
+Route::post('me', 'AuthController@me');
