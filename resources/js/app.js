b67b6541f5a71f50ade89ed83260fc8a80a657f1
roneydev01 <roneyfreitas.rf@gmail.com>
2021-09-07 00:36:39 -0300

    feat: Created route, view and mark component

--- resources/js/app.js
+++ resources/js/app.js
@@ -25,6 +25,7 @@ Vue.component(
 );
 Vue.component("login-component", require("./components/Login.vue").default);
 Vue.component("home-component", require("./components/Home.vue").default);
+Vue.component("marcas-component", require("./components/Marcas.vue").default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/Marcas.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Marcas</div>
+
+                    <div class="card-body">
+                        <p>Manipular registros de marcas.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {};
+</script>
