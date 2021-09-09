afd85cb26a15797013699cea1d4dfe5154b99625
roneydev01 <roneyfreitas.rf@gmail.com>
2021-09-08 23:30:59 -0300

    feat: Created search card for brands

--- public/js/app.js
+++ public/js/app.js
@@ -2077,6 +2077,45 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({});
 
 /***/ }),
@@ -38155,10 +38194,89 @@ var staticRenderFns = [
       _c("div", { staticClass: "row justify-content-center" }, [
         _c("div", { staticClass: "col-md-8" }, [
           _c("div", { staticClass: "card" }, [
-            _c("div", { staticClass: "card-header" }, [_vm._v("Marcas")]),
+            _c("div", { staticClass: "card-header" }, [
+              _vm._v("Busca de Marcas")
+            ]),
             _vm._v(" "),
             _c("div", { staticClass: "card-body" }, [
-              _c("p", [_vm._v("Manipular registros de marcas.")])
+              _c("div", { staticClass: "form-row" }, [
+                _c("div", { staticClass: "col mb-3" }, [
+                  _c(
+                    "label",
+                    { staticClass: "form-label", attrs: { for: "inputId" } },
+                    [_vm._v("ID")]
+                  ),
+                  _vm._v(" "),
+                  _c("input", {
+                    staticClass: "form-control",
+                    attrs: {
+                      type: "number",
+                      id: "inputId",
+                      "aria-describedby": "idHelp",
+                      placeholder: "ID"
+                    }
+                  }),
+                  _vm._v(" "),
+                  _c(
+                    "div",
+                    {
+                      staticClass: "form-text text-muted",
+                      attrs: { id: "idHelp" }
+                    },
+                    [
+                      _vm._v(
+                        "\n                                Opcional. Informe o ID do registro.\n                            "
+                      )
+                    ]
+                  )
+                ]),
+                _vm._v(" "),
+                _c("div", { staticClass: " col mb-3" }, [
+                  _c(
+                    "label",
+                    { staticClass: "form-label", attrs: { for: "inputNome" } },
+                    [_vm._v("Nome")]
+                  ),
+                  _vm._v(" "),
+                  _c("input", {
+                    staticClass: "form-control",
+                    attrs: {
+                      type: "text",
+                      id: "inputNome",
+                      "aria-describedby": "nomeHelp",
+                      placeholder: "Nome da marca"
+                    }
+                  }),
+                  _vm._v(" "),
+                  _c(
+                    "div",
+                    {
+                      staticClass: "form-text text-muted",
+                      attrs: { id: "nomeHelp" }
+                    },
+                    [
+                      _vm._v(
+                        "\n                                Opcional. Informe o nome da marca.\n                            "
+                      )
+                    ]
+                  )
+                ])
+              ])
+            ]),
+            _vm._v(" "),
+            _c("div", { staticClass: "card-footer" }, [
+              _c(
+                "button",
+                {
+                  staticClass: "btn btn-primary btn-sm float-right",
+                  attrs: { type: "submit" }
+                },
+                [
+                  _vm._v(
+                    "\n                        Pesquisar\n                    "
+                  )
+                ]
+              )
             ])
           ])
         ])
