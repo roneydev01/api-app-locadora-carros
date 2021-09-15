fe4fc206bdaa64c9adbcc211295b04b663399070
Roney Freitas <roneyfreitas.rf@gmail.com>
2021-09-15 00:12:42 -0300

    feat: Created card component

--- resources/js/app.js
+++ resources/js/app.js
@@ -31,6 +31,7 @@ Vue.component(
     require("./components/InputContainer.vue").default
 );
 Vue.component("table-component", require("./components/Table.vue").default);
+Vue.component("card-component", require("./components/Card.vue").default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
