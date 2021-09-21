74a80a7892381140f34c0abbcd48a108d3b92f23
Roney Freitas <roneyfreitas.rf@gmail.com>
2021-09-20 23:43:00 -0300

    feat: Adjusted table component for more dynamics

--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -2,14 +2,24 @@
     <table class="table table-striped table-hover">
         <thead>
             <tr>
-                <th scope="col" v-for="t, key in titulos" :key="key">{{ t }}</th>
+                <th scope="col" v-for="t, key in titulos" :key="key" class="text-text-uppercase">{{ t }}</th>
             </tr>
         </thead>
         <tbody>
-            <tr v-for="m in dados" :key="m.id">
-                <th scope="row">{{ m.id }}</th>
-                <td>{{ m.nome }}</td>
-                <td><img :src="'/storage/'+m.imagem" width="30" height="30"></td>
+            <!--Pega o obejto de dados do banco -->
+            <tr v-for="obj in dados" :key="obj.id">
+            <!--Verifica se os titulos passados pela props corresponde a chave do banco. Ex:id -->
+                <td
+                    v-if="titulos.includes(chave)"
+                    v-for="valor, chave in obj" :key="chave"
+                >
+                    <span v-if="chave == 'imagem'">
+                        <img :src="'/storage/'+valor" width="30" height="30">
+                    </span>
+                    <span v-else>
+                        {{ valor }}
+                    </span>
+                </td>
             </tr>
         </tbody>
     </table>
