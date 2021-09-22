f279600ec055e9d3f7d8ceb3e861f93bab5a2b03
Roney Freitas <roneyfreitas.rf@gmail.com>
2021-09-21 23:30:03 -0300

    feat: Adjusted and added metadata to titles

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -57,7 +57,11 @@
                     <template v-slot:conteudo>
                         <table-component
                             :dados="marcas"
-                            :titulos="['id', 'nome', 'imagem']"
+                            :titulos="{
+                                id: {titulo: 'ID', tipo: 'texto'},
+                                nome: {titulo: 'Nome', tipo: 'texto'},
+                                imagem: {titulo: 'Imagem', tipo: 'imagem'},
+                            }"
                         >
                         </table-component>
                     </template>
--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -2,22 +2,17 @@
     <table class="table table-striped table-hover">
         <thead>
             <tr>
-                <th scope="col" v-for="t, key in titulos" :key="key" class="text-text-uppercase">{{ t }}</th>
+                <th scope="col" v-for="t, key in titulos" :key="key" class="text-text-uppercase">{{ t.titulo }}</th>
             </tr>
         </thead>
         <tbody>
             <!--Pega o obejto de dados do banco -->
-            <tr v-for="obj in dados" :key="obj.id">
+            <tr v-for="obj, chave in dadosFiltrados" :key="chave">
             <!--Verifica se os titulos passados pela props corresponde a chave do banco. Ex:id -->
-                <td
-                    v-if="titulos.includes(chave)"
-                    v-for="valor, chave in obj" :key="chave"
-                >
-                    <span v-if="chave == 'imagem'">
+                <td v-for="valor, chaveValor in obj" :key="chaveValor">
+                    <span v-if="titulos[chaveValor].tipo == 'texto'">{{valor}}</span>
+                    <span v-if="titulos[chaveValor].tipo == 'imagem'">
                         <img :src="'/storage/'+valor" width="30" height="30">
-                    </span>
-                    <span v-else>
-                        {{ valor }}
                     </span>
                 </td>
             </tr>
@@ -26,6 +21,25 @@
 </template>
 <script>
 export default {
-    props: ['dados','titulos']
+    props: ['dados','titulos'],
+    computed: {
+        dadosFiltrados() {
+            let campos = Object.keys(this.titulos)
+            let dadosFiltrados = []
+            //console.log(campos)
+            //console.log(this.dados)
+            this.dados.map((item, chave) => {
+                //console.log(chave, item)
+                let itemFiltrado = {}
+                campos.forEach(campo => {
+                    //console.log(campo)
+                    itemFiltrado[campo] = item[campo]
+                })
+                //console.log(itemFiltrado)
+                dadosFiltrados.push(itemFiltrado)
+            })
+            return dadosFiltrados
+        }
+    }
 };
 </script>
